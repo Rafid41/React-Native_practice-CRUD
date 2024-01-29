4f8350b97d7b8fec1a12a70252e9b50387ff1db0
Rafid41 <wap.alnahiyan525@gmail.com>
2024-01-29 19:00:46 +0600

    <TextInput/> and onChangeText

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const handleInputChange = (text) => {
+        alert(text);
+    };
+    return (
+        <View style={styles.container}>
+            <Text>Hello world</Text>
+            <Text>second para</Text>
+            {/* placeholder props */}
+            <TextInput
+                style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+                placeholder="Give Input"
+                //eta ekta fn expect ore, ja pass korbo se ta fn er vitor pass kore dbe
+                onChangeText={(text) => handleInputChange(text)}
+            />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
