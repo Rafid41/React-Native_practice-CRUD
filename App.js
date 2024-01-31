b9e8841a10b82ed43901e570ca876274f4d4e08f
Rafid41 <wap.alnahiyan525@gmail.com>
2024-01-31 23:32:29 +0600

    installing navigation dependancies

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 // App.js
+import "react-native-gesture-handler"; // file er surute ei import thakbe
 import React from "react";
 import MainComponent from "./src/MainComponent";
 import { Provider } from "react-redux";
 import Store from "./src/redux/store";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/Components/Login/Login";
 
+// main stack  //stack navigationk stack akare shajabe
+const Stack = createStackNavigator();
 export default function App() {
     return (
-        <Provider store={Store}>
-            <MainComponent />
-        </Provider>
+        <NavigationContainer>
+            <Provider store={Store}>
+                {/* Routing ekhane */}
+                <Stack.Navigator>
+                    {/* name="Login" navbar e login dekhabe name */}
+                    <Stack.Screen name="Login" component={Login} />
+                </Stack.Navigator>
+            </Provider>
+        </NavigationContainer>
     );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,14 @@
+// src\Components\Login\Login.js
+
+import React from "react";
+import { View, Text } from "react-native";
+
+const Login = () => {
+    return (
+        <View>
+            <Text>Login</Text>
+        </View>
+    );
+};
+
+export default Login;
