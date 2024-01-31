bd55e3a3d2f5e83e3a600f65df7dff431a3cca6c
Rafid41 <wap.alnahiyan525@gmail.com>
2024-02-01 01:38:49 +0600

    add icons to bottom nav bar

--- src/Components/NavigationTab/NavigationTab.js
+++ src/Components/NavigationTab/NavigationTab.js
@@ -1,17 +1,36 @@
 // src\Components\NavigationTab\NavigationTab.js
 import React from "react";
-import { View, Text } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SharePlaces from "../SharePlaces/SharePlaces";
 import FindPlaces from "../FindPlaces/FindPlaces";
+import Icons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 const NavigationTab = (props) => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Share Places" component={SharePlaces} />
-            <Tab.Screen name="Find Places" component={FindPlaces} />
+            <Tab.Screen
+                name="Find Places"
+                component={FindPlaces}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icons name="map" color={color} size={size} />
+                    ),
+                }}
+            />
+
+            <Tab.Screen
+                name="Share Places"
+                component={SharePlaces}
+                // show icons
+                options={{
+                    // default color and size
+                    tabBarIcon: ({ color, size }) => (
+                        <Icons name="people" color={color} size={size} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 };
