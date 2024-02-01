b06fcd1445d27ee02a90d535378c94496b66670e
Rafid41 <wap.alnahiyan525@gmail.com>
2024-02-02 02:49:21 +0600

    firebase initialization, firebase database as REST API

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,10 +1,24 @@
 // src\redux\actionCreators.js
 import * as actionTypes from "./actionTypes";
 
+// firebase database e upload
+// axios o use kora jay
+// ekhane fetch use kora holo
 export const addPlace = (place) => {
-    return {
-        type: actionTypes.ADD_PLACE,
-        payload: place,
+    return (disatch) => {
+        // places table e store
+        // link/uri , additional data
+        fetch(
+            "https://my-place-react-native-default-rtdb.asia-southeast1.firebasedatabase.app/places.json",
+            {
+                method: "POST",
+                body: JSON.stringify(place),
+            }
+        )
+            .catch((error) => console.log(error))
+            // error na hole
+            .then((response) => response.json())
+            .then((data) => console.log(data));
     };
 };
 
